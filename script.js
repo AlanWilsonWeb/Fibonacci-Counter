5bb27a4bdf0af1eae8dc506c314c7310043eba07
Alan Wilson <nannerfox@me.com>
2018-03-21 22:14:15 -0500

    Final
    Counts in 20 iterations.

---
+++ script.js
@@ -0,0 +1,23 @@
+var count = 0;
+var a = 1;
+var outCount = [1];
+let listRes = document.getElementById("result");
+
+while(count <= 20){
+  outCount.push(a);
+  a += outCount[count];
+  count++;
+};
+
+outCount.shift()
+
+function createList(outCount) {
+for (var f = 0; f < outCount.length; f++) {
+  var itemZ = outCount[f]
+  var listItem = document.createElement("LI");
+  listItem.textContent = itemZ;
+  listRes.appendChild(listItem);
+  }
+};
+
+createList(outCount);
